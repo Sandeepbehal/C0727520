ca815c33a4af9e32e6c30c093ef3eb132349d6d6
c0727520 <46795526+Sandeepbehal@users.noreply.github.com>
2019-03-28 16:29:06 -0400

    4:30 commit

--- C0727520/Program.cs
+++ C0727520/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace C0727520
+namespace PeterIsTheRulerOfIT
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Peter Rules the IT Universe!");
+Download();
+Console.ReadLine();
+}
+
+static void Download()
+{
+Task.Run(() => {
+Thread.Sleep(3000);
+Console.WriteLine("Download Complete ...");
+}
+);
+}
+}
 }
